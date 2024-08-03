72f6f30c9b0380006bdaf70d6ca6fce010745984
Gerardo02 <gerardo.jerez@hotmail.com>
2024-08-02 18:30:17 -0700

    help

--- src/components/charts/lines.tsx
+++ src/components/charts/lines.tsx
@@ -1,11 +1,9 @@
 'use client'
-import { TrendingUp } from 'lucide-react'
 import { CartesianGrid, LabelList, Line, LineChart, XAxis } from 'recharts'
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
@@ -85,14 +83,6 @@ export function LineComponent() {
           </LineChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter>
     </Card>
   )
 }
--- src/components/ui/MainHeader.tsx
+++ src/components/ui/MainHeader.tsx
@@ -70,7 +70,7 @@ const MainHeader: React.FC<HeaderProps> = ({
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
               <Input
                 type="search"
-                placeholder="Buscar productos..."
+                placeholder="Buscar..."
                 className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
               />
             </div>
